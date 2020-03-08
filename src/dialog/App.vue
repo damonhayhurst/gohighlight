ca823cc5846c1aaa107020e7d6fc0be87095b63e
Damon <damonhayhurst@gmail.com>
2020-03-08 22:37:22 +0000

    second commit

--- src/dialog/App.vue
+++ src/dialog/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
+    <ul style="list-style-type: none">
+      <li v-for="field in fields" :key="field.name">
     <b-form-group
         id="input-group-1"
-        label="Company name"
+        :label="field.name"
         label-for="input-1"
       >
         <b-form-input
           id="input-1"
           type="text"
-          required
-          v-model="value"
+          required 
+          v-model="field.value"
         ></b-form-input>
       </b-form-group>
+    </li>
+  </ul>
+    </v-container>
   </div>
 </template>
 
@@ -19,19 +24,35 @@
 export default {
   data () {
     return {
-      value: ""
+      fields: [
+        {name: "Company Name", value: ""},
+        {name: "Email", value: ""},
+        {name: "Phone", value: ""}
+      ],
+      currentValue: "",
+      fieldIndex: 0 
     }
   },
   methods: {
-    setValue: function (newValue) {
-      this.value = newValue
+    setCurrentValue: function (newCurrentValue) {
+      this.currentValue = newCurrentValue
+    }
+  },
+  watch: {
+    currentValue: function (newValue, oldValue) {
+      this.fields[this.fieldIndex].value = newValue
+      if (this.fieldIndex < this.fields.length ){
+        this.fieldIndex++
+      } else {
+        this.fieldIndex = 0 
+      }
     }
   },
   mounted: function () {
     var parent = this
     chrome.runtime.sendMessage({from:"dialog", listen:"true"})
     chrome.runtime.onMessage.addListener(function(msg) {
-      parent.setValue(msg.text)
+      parent.setCurrentValue(msg.text)
     })
   }
 }
@@ -40,8 +61,5 @@ export default {
 <style lang="scss" scoped>
 p {
   font-size: 20px;
-}
-div {
-
 }
 </style>
